6afd05881ca32e4b13a29502205ddfd0243a8595
matt-lio <matthewliogas@gmail.com>
2026-04-01 12:51:15 -0400

    Modified signextender.vhd to imm_gen.vhd: takes care of different types of instructions (I-type, B-type...) and shuffles bits accordingly. DOESN'T TAKE CARE OF ZERO EXTENDING, shouldn't need to since zero extending is only required for LBU and LHU instructions, which can be dealt with during MEM or WB stages.

--- imm_gen.vhd
+++ imm_gen.vhd
@@ -3,22 +3,52 @@ use IEEE.std_logic_1164.all;
 
 entity imm_gen is
     port(
-        imm_in: in std_logic_vector(11 downto 0);
-        zero_ext: in std_logic; --1 for zero extending, 0 for sign extending
+        instruction: in std_logic_vector(31 downto 0);
         imm_out: out std_logic_vector(31 downto 0)
     );
 end imm_gen;
 
 architecture behaviour of imm_gen is
+    signal opcode: std_logic_vector(6 downto 0);
 begin
-    process(imm_in,zero_ext)
+    process(instruction,opcode)
     begin
-        if zero_ext='1' then --we want to zero extend
-            imm_out(11 downto 0)<=imm_in;
-            imm_out(31 downto 12)<=(others=>'0');
-        else --we want to sign extend
-            imm_out(11 downto 0)<=imm_in;
-            imm_out(31 downto 12)<=(others=>imm_in(11));
-        end if;
+        imm_out<=(others=>'0');
+        case opcode is
+            -- I-type: ALU immediates, LW, JALR
+            when "0010011" | "0000011" | "1100111" =>
+                imm_out(31 downto 12)<=(others=>instruction(31)); --sign extend
+                imm_out(11 downto 0)<=(others=>instruction(31 downto 20));
+            
+            -- S-type: SW
+            when "0100011" =>
+                imm_out(31 downto 12)<=(others=>instruction(31)); --sign extend
+                imm_out(11 downto 5)<=(others=>instruction(31 downto 25));
+                imm_out(4 downto 0)<=(others=>instruction(11 downto 7));
+
+            -- B-type: Branch instructions
+            when "1100011" =>
+                imm_out(31 downto 12)<=(others=>instruction(31)); --sign extend
+                imm_out(11)<=instruction(7);
+                imm_out(10 downto 5)<=instruction(30 downto 25);
+                imm_out(4 downto 1)<=instruction(11 downto 8);
+                imm_out(0)<='0'; --even address
+            
+            -- J-type: JAL
+            when "1101111" =>
+                imm_out(31 downto 12)<=(others=>instruction(31)); --sign extend
+                imm_out(19 downto 12)<=instruction(19 downto 12);
+                imm_out(11)<=instruction(20);
+                imm_out(10 downto 1)<=instruction(30 downto 21);
+                imm_out(0)<='0'; --even address
+            
+            -- U-type: LUI, AUIPC
+            when "0110111" | "0010111" =>
+                imm_out(31 downto 12)<=instruction(31 downto 12);
+                imm_out(11 downto 0)<=(others=>'0'); 
+            
+            when others =>
+                imm_out<=(others=>'0');
+        end case;
     end process;
 end behaviour;
