d82f55baeaa942c09f85b8d75e1145d898e105d0
Aurélia Chu Bouliane <aurelia.bouliane@gmail.com>
2026-04-12 18:10:02 -0400

    added vcom 2008 as per TA instructions in teams

--- testbench.tcl
+++ testbench.tcl
@@ -13,22 +13,22 @@ vmap work work
 puts "\nCompiling VHDL files..."
 
 # Compile in dependency order
-vcom -work work adder.vhd
-vcom -work work alu.vhd
-vcom -work work control.vhd
-vcom -work work generic_register.vhd
-vcom -work work register.vhd
-vcom -work work memory.vhd
-vcom -work work MUX_2_1.vhd
-vcom -work work MUX_2_1_32bit.vhd
-vcom -work work register_file.vhd
-vcom -work work imm_gen.vhd
-vcom -work work if_id_register.vhd
-vcom -work work id_ex_register.vhd
-vcom -work work ex_mem_register.vhd
-vcom -work work mem_wb_register.vhd
-vcom -work work control_hazard.vhd
-vcom -work work processor.vhd
+vcom -2008 -work work adder.vhd
+vcom -2008 -work work alu.vhd
+vcom -2008 -work work control.vhd
+vcom -2008 -work work generic_register.vhd
+vcom -2008 -work work register.vhd
+vcom -2008 -work work memory.vhd
+vcom -2008 -work work MUX_2_1.vhd
+vcom -2008 -work work MUX_2_1_32bit.vhd
+vcom -2008 -work work register_file.vhd
+vcom -2008 -work work imm_gen.vhd
+vcom -2008 -work work if_id_register.vhd
+vcom -2008 -work work id_ex_register.vhd
+vcom -2008 -work work ex_mem_register.vhd
+vcom -2008 -work work mem_wb_register.vhd
+vcom -2008 -work work control_hazard.vhd
+vcom -2008 -work work processor.vhd
 
 puts "✓ Compilation complete\n"
 
